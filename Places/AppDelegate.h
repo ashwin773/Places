883dab6810469c85bf3dc9475a098e8951c0b83f
ashwin shrestha <ashwinshrestha06@gmail.com>
2016-02-29 15:35:38 +0545

    Initial Commit

---
+++ Places/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Places
+//
+//  Created by Ebpearls on 2/29/16.
+//  Copyright © 2016 Ebpearls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Places/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Places
+//
+//  Created by Ebpearls on 2/29/16.
+//  Copyright © 2016 Ebpearls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
